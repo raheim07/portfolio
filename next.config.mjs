42ab69b80eac56c2b17d3e921b220ee88dafb81f
raheim07 <raheimhoiletth@gmail.com>
2026-02-19 17:06:07 -0500

    Out Folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 👈 VERY IMPORTANT
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
+
+  basePath: '/portfolio', // 👈 replace with your repo name
 }
 
 export default nextConfig
