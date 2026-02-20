d8ceb52d7d10ee8cf71fc1a689de7e5646b94ecb
raheim07 <raheimhoiletth@gmail.com>
2026-02-20 08:41:41 -0500

    new change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from 'next'
 import { Inter, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-
+// Here
 const _inter = Inter({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
